8d8ab62881bdbcac6ce62d840ce08eeb2a0970c8
Visheshgpt <47291811+Visheshgpt@users.noreply.github.com>
2024-11-04 18:01:09 +0530

    Update const.ts

--- packages/farms/src/const.ts
+++ packages/farms/src/const.ts
@@ -4,6 +4,7 @@ import { getFullDecimalMultiplier } from './getFullDecimalMultiplier'
 export const FIXED_ZERO = FixedNumber.from(0)
 export const FIXED_ONE = FixedNumber.from(1)
 export const FIXED_TWO = FixedNumber.from(2)
+export const FIXED_THREE = FixedNumber.from(3)
 
 export const FIXED_TEN_IN_POWER_18 = FixedNumber.from(getFullDecimalMultiplier(18))
 
